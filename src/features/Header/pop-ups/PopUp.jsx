8a39676a9d338af8f56c0b83e7a2376d0262adaa
Mikhail Skrobat <obgora228@gmail.com>
2024-12-24 18:53:28 +0100

    Hero + Header, but without pop up

---
+++ src/features/Header/pop-ups/PopUp.jsx
@@ -0,0 +1,7 @@
+import {useState} from "react";
+
+const PopUp = () => {
+    const [isOpen, setIsOpen] = useState(false);
+}
+
+export default PopUp;
--- src/features/HeroSection/ui/InformationSection/InformationSection.jsx
+++ src/features/HeroSection/ui/InformationSection/InformationSection.jsx
@@ -1,45 +1,54 @@
-import {useEffect, useState} from "react";
-import './InformationSection.scss'
+import { useEffect, useState } from "react";
+import "./InformationSection.scss";
 
 const InformationSection = () => {
+    const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 1440);
 
-    const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 1440)
+    const images = [
+        "images/client-databiz.svg",
+        "images/client-audiophile.svg",
+        "images/client-meet.svg",
+        "images/client-maker.svg",
+    ];
 
     useEffect(() => {
         const handleWindowResize = () => {
-            setIsWideScreen(window.innerWidth > 1440)
+            setIsWideScreen(window.innerWidth > 1440);
         };
 
         window.addEventListener('resize', handleWindowResize);
-        return () => {
-            window.removeEventListener('resize', handleWindowResize);
-        }
-    }, [])
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
 
     return (
+        <div className="information-grid">
+            <div className="text-container">
+                <h1>
+                    Make
+                    {isWideScreen ? <br /> : " "}
+                    remote work
+                </h1>
+                <p>
+                    Get your team in sync, no matter your location.<br />
+                    Streamline processes, create team rituals, and<br />
+                    watch productivity soar.
+                </p>
+                <button>Learn more</button>
 
-        <>
-            <div className="information-container">
-                <div className="text-container">
-                    <h1>
-                        Make
-                        {isWideScreen ? <br/> : " "}
-                        remote work
-                    </h1>
-                    <p>
-                        Get your team in sync, no matter your location.<br/>
-                        Streamline processes, create team rituals, and<br/>
-                        watch productivity soar.
-                    </p>
-                    <button>Learn more</button>
+                <div className="client-logos">
+                    {images.map((src, index) => (
+                        <img
+                            key={index}
+                            src={src}
+                            className="client-logo"
+                        />
+                    ))}
                 </div>
-
             </div>
-            <div className="background">
 
-            </div>
-        </>
-    )
+            <div className="background"></div>
+        </div>
+    );
 }
 
 export default InformationSection;
