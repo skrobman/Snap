743e5096a0a945a848c1b361ba5439001f0d5ec7
Mikhail Skrobat <obgora228@gmail.com>
2024-12-25 02:47:54 +0100

    hh

--- src/features/Header/ui/Navigation/NavItem/NavItem.jsx
+++ src/features/Header/ui/Navigation/NavItem/NavItem.jsx
@@ -1,26 +1,77 @@
-import '../NavStyles/navigation.scss'
+import '../NavStyles/navigation.scss';
+import FeaturesMenu from '../../../pop-ups/FeaturesMenu';
+import CompanyMenu from "../../../pop-ups/CompanyMenu";
+import { useState } from 'react';
 
-const NavItem = ({label, hasDropdown}) => {
+const NavItem = ({ label, hasDropdown, isFeature = false, isCompany = false }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <a className="nav-link">
-            {label}
+        <div
+            className="nav-link"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <span>{label}</span>
             {hasDropdown && (
                 <svg
-                    className="dropdown-icon"
                     width="10"
                     height="6"
                     xmlns="http://www.w3.org/2000/svg"
+                    className={(isFeature && hasDropdown && isHovered) ||
+                    (isCompany && hasDropdown && isHovered) ? "none dropdown-icon" : "dropdown-icon show"}
+
                 >
-                    <path className="dropdown-path"
-                          stroke="#686868"
-                          strokeWidth="1.5"
-                          fill="none"
-                          d="m1 1 4 4 4-4"
+                    <path
+                        stroke="#686868"
+                        strokeWidth="1.5"
+                        fill="none"
+                        d="m1 1 4 4 4-4"
                     />
                 </svg>
             )}
-        </a>
+
+            {isFeature && hasDropdown && isHovered && (
+                <>
+                    <FeaturesMenu />
+                    <svg
+                        width="10"
+                        height="6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="dropdown-icon"
+                    >
+                        <path
+                            stroke="#686868"
+                            strokeWidth="1.5"
+                            fill="none"
+                            d="m1 5 4-4 4 4"
+                        />
+                    </svg>
+
+                </>
+
+            )}
+            {isCompany && hasDropdown && isHovered && (
+                <>
+                    <CompanyMenu />
+                    <svg
+                        width="10"
+                        height="6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="dropdown-icon"
+                    >
+                        <path
+                            stroke="#686868"
+                            strokeWidth="1.5"
+                            fill="none"
+                            d="m1 5 4-4 4 4"
+                        />
+                    </svg>
+
+                </>
+            )}
+        </div>
     );
 };
 
-export default NavItem
+export default NavItem;
