f91e527b6e2de28517c7b1035c2c8dd91bca4105
Mikhail Skrobat <obgora228@gmail.com>
2024-12-26 01:42:01 +0100

    Need to change menu to make it done

--- src/features/Header/ui/Header.jsx
+++ src/features/Header/ui/Header.jsx
@@ -2,23 +2,19 @@ import Logo from './Logo/Logo'
 import NavContainer from './Navigation/NavContainer/NavContainer'
 import RegistrButton from './Navigation/RegistrButton/RegistrButton'
 import BurgerMenu from './Navigation/BurgerMenu/BurgerMenu'
-
 import './Navigation/NavStyles/navigation.scss'
 
 const Header = () => {
     return (
-        <>
-            <header className="header-container">
-                <div className="nav">
-                    <Logo/>
-
-                    <NavContainer/>
-                    <RegistrButton/>
-                    <BurgerMenu />
-                </div>
-            </header>
-        </>
+        <header className="header-container">
+            <nav className="nav">
+                <Logo/>
+                <NavContainer/>
+                <RegistrButton/>
+                <BurgerMenu/>
+            </nav>
+        </header>
     )
 }
 
-export default Header;
+export default Header
